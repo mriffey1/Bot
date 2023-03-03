1207c31e19bfad4e5fa6bbae7429018833b567b5
MeganRiffey <mriffe4@wgu.edu>
2023-03-02 19:21:10 -0500

    first commit

---
+++ main_bot.py
@@ -0,0 +1,26 @@
+import discord
+import os
+import random
+from dotenv import load_dotenv
+from discord.ext import commands
+
+load_dotenv()
+token = os.getenv('DISCORD_TOKEN')
+intents = discord.Intents().all()
+
+intents.message_content = True
+bot = discord.Bot()
+
+@bot.event
+async def on_ready():
+    print("Logged in as a bot {0.user}".format(bot))
+
+@bot.command(name="firstslash", description="Sends the bot's latency.")
+async def first_slash(ctx): 
+    await ctx.respond("You executed the slash command!")
+
+@bot.command(description="Sends the bot's latency.")
+async def test(ctx): 
+    await ctx.respond("You executed the slash command!")
+
+bot.run(token)
